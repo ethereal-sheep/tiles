e9620951b6a93d2139248404358334791df0df52
Cantius Chew <cantiusc@blackmagicdesign.com>
2026-06-16 11:27:13 +0800

    remove panes

--- crates/panes/src/widget.rs
+++
@@ -1,9 +0,0 @@
-use tiles::Cell;
-
-use crate::config::PaneStyle;
-use crate::layout::Rect;
-
-pub trait Widget {
-    fn size(&self, style: &PaneStyle) -> (f32, f32);
-    fn render(&self, rect: Rect, style: &PaneStyle, hovered: bool, active: bool) -> Vec<Cell>;
-}
